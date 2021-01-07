d5271ebe4dc39dff89acd416885f975272d2f84c
Sarah Hoffman <74530173+jadedragon21@users.noreply.github.com>
2021-01-07 17:57:08 -0600

    errors galore
    more errors:
    not a statement identifiers expected
    not a statement identifiers expected and String should be capitalized

--- src/com/tts/HelloWorld.java
+++ src/com/tts/HelloWorld.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 // Your First Program
 
 public class HelloWorld {
-    public static void main(String[void] args) {
+    public static void main(String[] args) {
         //        .print vs. println
         //        System.out.print("Hello, World!\n");
         //        System.out.print("Today is awesome!");
@@ -15,7 +15,9 @@ public class HelloWorld {
         String lastName = userInput.nextLine();
         System.out.printf("Hello %s %s!\n", firstName, lastName);
         System.out.printf("Goodbye %s, %s!\n", lastName, firstName);
-
+       //int int = 3; //not a statement identifiers expected
+        //double double class= 99.0 //not a statement identifiers expected
+        //string void ="does this work"; ////not a statement identifiers expected and String should be capitalized
     }
 }
 
