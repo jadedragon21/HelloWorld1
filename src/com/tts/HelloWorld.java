a95c4b1849a5a2704650e3632876856c04f374f0
Sarah Hoffman <74530173+jadedragon21@users.noreply.github.com>
2021-01-07 17:37:46 -0600

    java: cannot find symbol symbol:   variable userInput
    result of not putting in the userInput correctly on line 11.

--- src/com/tts/HelloWorld.java
+++ src/com/tts/HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld {
         //        System.out.print("Hello, World!\n");
         //        System.out.print("Today is awesome!");
         //        System.out.print("... don't you think?");
-        Scanner Input = new Scanner(System.in);
+        Scanner userInput = new Scanner(System.in);
         System.out.println("What is your first name?");
         String firstName = userInput.nextLine();
         System.out.println("What is your last name?");
