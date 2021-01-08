c6e3162d388c1bf83d4cb17fc2c8624e66adba2b
Sarah Hoffman <74530173+jadedragon21@users.noreply.github.com>
2021-01-07 21:08:53 -0600

    error funny
    gas is dangerous. getting local variables to work can be childishly funny.

---
+++ src/com/tts/ErrorReport.java
@@ -0,0 +1,28 @@
+package com.tts;
+
+public class ErrorReport {
+    public static void main(String[] args) {
+//illegal field names
+//byte nybbles = 000bff10_0404; //byte a = 100;
+//int int = 1; //int a = 5;
+//int char = 'a'; //correct form should be char char1 = 'a';
+//double double d1= 123.4; //double d1 = 123.4
+//boolean boolean = true; //boolean one = true
+//uninitialized fields with printing
+//int thisIsntGoingToInitialize;
+//        System.out.println(thisIsntGoingToInitialize);
+//did not initialize or work...
+//local variable
+        String iNeedNoBurritos;
+        {
+            iNeedNoBurritos = "Gas is dangerous!";
+        }
+        System.out.println("Come on you know you wanna laugh!" + iNeedNoBurritos);
+        //this one works and is funny dunno about legal though lol
+        {
+            String doIworkInHere;
+            System.out.println("Is this working?" + doIworkInHere);
+            //not intialized properly since the string should've been out of the {} and above the first {
+        }
+}
+}
