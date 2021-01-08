f2deb3dc8ae07746735e941849ebe6be03a891bb
Sarah Hoffman <74530173+jadedragon21@users.noreply.github.com>
2021-01-07 18:04:54 -0600

    pressed %ttt instead of %s on line27
    Exception in thread "main" java.util.UnknownFormatConversionException:Conversion = 'tt'
    at java.util.Formatter$FormatSpecifier.checkDateTime(Formatter.java:2963)
    at java.util.Formatter$FormatSpecifier.<init>(Formatter.java:2723)
    at java.util.Formatter.parse(Formatter.java:2560)
    at java.util.Formatter.format(Formatter.java:2501)
    at java.io.PrintStream.format(PrintStream.java:970)
    at java.io.PrintStream.printf(PrintStream.java:871)
    at com.tts.MadLib.main(MadLib.java:27)

--- src/com/tts/HelloWorld.java
+++ src/com/tts/HelloWorld.java
@@ -8,16 +8,19 @@ public class HelloWorld {
         //        System.out.print("Hello, World!\n");
         //        System.out.print("Today is awesome!");
         //        System.out.print("... don't you think?");
-        Scanner userInput = new Scanner(System.in);
-        System.out.println("What is your first name?");
-        String firstName = userInput.nextLine();
-        System.out.println("What is your last name?");
-        String lastName = userInput.nextLine();
-        System.out.printf("Hello %s %s!\n", firstName, lastName);
-        System.out.printf("Goodbye %s, %s!\n", lastName, firstName);
+//        Scanner userInput = new Scanner(System.in);
+//        System.out.println("What is your first name?");
+//        String firstName = userInput.nextLine();
+//        System.out.println("What is your last name?");
+//        String lastName = userInput.nextLine();
+//        System.out.printf("Hello %s %s!\n", firstName, lastName);
+//        System.out.printf("Goodbye %s, %s!\n", lastName, firstName);
        //int int = 3; //not a statement identifiers expected
         //double double class= 99.0 //not a statement identifiers expected
         //string void ="does this work"; ////not a statement identifiers expected and String should be capitalized
+
+
+
     }
 }
 
