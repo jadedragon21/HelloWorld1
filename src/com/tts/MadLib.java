dfa90370996ef06ee6e398140651bc6313a4d5c4
Sarah Hoffman <74530173+jadedragon21@users.noreply.github.com>
2021-01-07 18:16:07 -0600

    //took out one %s on line 25
    not an error but somethin g like this can be missed more like human error.
    so the first madlib paragraph changed.

--- src/com/tts/MadLib.java
+++ src/com/tts/MadLib.java
@@ -20,9 +20,9 @@ public class MadLib {
         System.out.println("Give me your name: ");
         String yourName = userInput.nextLine();
 
-        System.out.printls("Here is your story: /n");
+        System.out.printf("Here is your story: \n");
         System.out.printf("\n Dear %s,", name);
-        System.out.printf("\n You are %s %s and I want to be your %s!", adverb, adjective, noun);
+        System.out.printf("\n You are %s  and I want to be your %s!", adverb, adjective, noun); //took out one %s
         System.out.printf("\n I want to go to the %s with you in %s days.", nounTwo, number);
         System.out.printf("\n Sincerely, %s \n", yourName);
 
@@ -46,5 +46,29 @@ public class MadLib {
 //        You are running pretty and I want to be your banana!
 //                I want to go to the cart with you in 1 days.
 //                Sincerely, Simeon C. Sayaz
+        //became
+//        Enter a name:
+//        boogie
+//        Give me an adverb:
+//        running
+//        Give me an adjective:
+//        pretty
+//        Give me a noun:
+//        banana
+//        Give me another noun:
+//        cart
+//        Give me a number:
+//        1
+//        Give me your name:
+//        simeon c sawyer
+//        Here is your story:
+//
+//        Dear boogie,
+//        You are running  and I want to be your pretty!
+//                I want to go to the cart with you in 1 days.
+//                Sincerely, simeon c sawyer
+//
+//        Process finished with exit code 0
+
     }
 }
